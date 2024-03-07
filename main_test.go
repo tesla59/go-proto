2ace0715c96d40791498db586a5d3f5d2fd49e3c
tesla59 <nishant@heim.id>
2024-03-07 23:32:22 +0530

    refactor benchmark
    Signed-off-by: tesla59 <nishant@heim.id>

--- main_test.go
+++ main_test.go
@@ -3,29 +3,55 @@ package main
 import (
 	"testing"
 
-	"github.com/tesla59/go-proto/pb/simple"
+	"github.com/tesla59/go-proto/pb/V2"
+	"github.com/tesla59/go-proto/pb/V2vtproto"
 	"google.golang.org/protobuf/proto"
 )
 
-var SimpleMessage = simple.SimpleMessage{
-	Id:      1234,
-	Message: "This is a simple message",
-}
-
 func BenchmarkSerialize(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		_, err := proto.Marshal(&SimpleMessage)
+		_, err := proto.Marshal(&V2.SimpleMessage{})
 		if err != nil {
 			b.Error("Error while marshalling", err)
 		}
 	}
 }
 
+func BenchmarkDeserialize(b *testing.B) {
+	data, err := proto.Marshal(&V2.SimpleMessage{})
+	if err != nil {
+		b.Error("Error while marshalling", err)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var sm V2.SimpleMessage
+		err := proto.Unmarshal(data, &sm)
+		if err != nil {
+			b.Error("Error while unmarshalling", err)
+		}
+	}
+}
+
 func BenchmarkSerializeVT(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		_, err := SimpleMessage.MarshalVT()
+		_, err := (&V2vtproto.SimpleMessage{}).MarshalVT()
 		if err != nil {
 			b.Error("Error while marshalling", err)
+		}
+	}
+}
+
+func BenchmarkDeserializeVT(b *testing.B) {
+	data, err := (&V2vtproto.SimpleMessage{}).MarshalVT()
+	if err != nil {
+		b.Error("Error while marshalling", err)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var sm V2vtproto.SimpleMessage
+		err := sm.UnmarshalVT(data)
+		if err != nil {
+			b.Error("Error while unmarshalling", err)
 		}
 	}
 }
--- pb/V2/SimpleMessage.pb.go
+++ pb/V2/SimpleMessage.pb.go
@@ -2,9 +2,9 @@
 // versions:
 // 	protoc-gen-go v1.32.0
 // 	protoc        v4.25.3
-// source: proto/SimpleMessage.proto
+// source: proto/V2/SimpleMessage.proto
 
-package simple
+package V2
 
 import (
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
@@ -32,7 +32,7 @@ type SimpleMessage struct {
 func (x *SimpleMessage) Reset() {
 	*x = SimpleMessage{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_proto_SimpleMessage_proto_msgTypes[0]
+		mi := &file_proto_V2_SimpleMessage_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -45,7 +45,7 @@ func (x *SimpleMessage) String() string {
 func (*SimpleMessage) ProtoMessage() {}
 
 func (x *SimpleMessage) ProtoReflect() protoreflect.Message {
-	mi := &file_proto_SimpleMessage_proto_msgTypes[0]
+	mi := &file_proto_V2_SimpleMessage_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -58,7 +58,7 @@ func (x *SimpleMessage) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use SimpleMessage.ProtoReflect.Descriptor instead.
 func (*SimpleMessage) Descriptor() ([]byte, []int) {
-	return file_proto_SimpleMessage_proto_rawDescGZIP(), []int{0}
+	return file_proto_V2_SimpleMessage_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *SimpleMessage) GetId() int32 {
@@ -75,35 +75,36 @@ func (x *SimpleMessage) GetMessage() string {
 	return ""
 }
 
-var File_proto_SimpleMessage_proto protoreflect.FileDescriptor
+var File_proto_V2_SimpleMessage_proto protoreflect.FileDescriptor
 
-var file_proto_SimpleMessage_proto_rawDesc = []byte{
-	0x0a, 0x19, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x53, 0x69, 0x6d, 0x70, 0x6c, 0x65, 0x4d, 0x65,
-	0x73, 0x73, 0x61, 0x67, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x22, 0x39, 0x0a, 0x0d, 0x53,
-	0x69, 0x6d, 0x70, 0x6c, 0x65, 0x4d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x12, 0x0e, 0x0a, 0x02,
-	0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x05, 0x52, 0x02, 0x69, 0x64, 0x12, 0x18, 0x0a, 0x07,
-	0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x6d,
-	0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x42, 0x0b, 0x5a, 0x09, 0x70, 0x62, 0x2f, 0x73, 0x69, 0x6d,
-	0x70, 0x6c, 0x65, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+var file_proto_V2_SimpleMessage_proto_rawDesc = []byte{
+	0x0a, 0x1c, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x56, 0x32, 0x2f, 0x53, 0x69, 0x6d, 0x70, 0x6c,
+	0x65, 0x4d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x05,
+	0x70, 0x62, 0x2e, 0x56, 0x32, 0x22, 0x39, 0x0a, 0x0d, 0x53, 0x69, 0x6d, 0x70, 0x6c, 0x65, 0x4d,
+	0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01,
+	0x28, 0x05, 0x52, 0x02, 0x69, 0x64, 0x12, 0x18, 0x0a, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67,
+	0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65,
+	0x42, 0x07, 0x5a, 0x05, 0x70, 0x62, 0x2f, 0x56, 0x32, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f,
+	0x33,
 }
 
 var (
-	file_proto_SimpleMessage_proto_rawDescOnce sync.Once
-	file_proto_SimpleMessage_proto_rawDescData = file_proto_SimpleMessage_proto_rawDesc
+	file_proto_V2_SimpleMessage_proto_rawDescOnce sync.Once
+	file_proto_V2_SimpleMessage_proto_rawDescData = file_proto_V2_SimpleMessage_proto_rawDesc
 )
 
-func file_proto_SimpleMessage_proto_rawDescGZIP() []byte {
-	file_proto_SimpleMessage_proto_rawDescOnce.Do(func() {
-		file_proto_SimpleMessage_proto_rawDescData = protoimpl.X.CompressGZIP(file_proto_SimpleMessage_proto_rawDescData)
+func file_proto_V2_SimpleMessage_proto_rawDescGZIP() []byte {
+	file_proto_V2_SimpleMessage_proto_rawDescOnce.Do(func() {
+		file_proto_V2_SimpleMessage_proto_rawDescData = protoimpl.X.CompressGZIP(file_proto_V2_SimpleMessage_proto_rawDescData)
 	})
-	return file_proto_SimpleMessage_proto_rawDescData
+	return file_proto_V2_SimpleMessage_proto_rawDescData
 }
 
-var file_proto_SimpleMessage_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
-var file_proto_SimpleMessage_proto_goTypes = []interface{}{
-	(*SimpleMessage)(nil), // 0: SimpleMessage
+var file_proto_V2_SimpleMessage_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_proto_V2_SimpleMessage_proto_goTypes = []interface{}{
+	(*SimpleMessage)(nil), // 0: pb.V2.SimpleMessage
 }
-var file_proto_SimpleMessage_proto_depIdxs = []int32{
+var file_proto_V2_SimpleMessage_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
 	0, // [0:0] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -111,13 +112,13 @@ var file_proto_SimpleMessage_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_proto_SimpleMessage_proto_init() }
-func file_proto_SimpleMessage_proto_init() {
-	if File_proto_SimpleMessage_proto != nil {
+func init() { file_proto_V2_SimpleMessage_proto_init() }
+func file_proto_V2_SimpleMessage_proto_init() {
+	if File_proto_V2_SimpleMessage_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_proto_SimpleMessage_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_proto_V2_SimpleMessage_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*SimpleMessage); i {
 			case 0:
 				return &v.state
@@ -134,18 +135,18 @@ func file_proto_SimpleMessage_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_proto_SimpleMessage_proto_rawDesc,
+			RawDescriptor: file_proto_V2_SimpleMessage_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   1,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
-		GoTypes:           file_proto_SimpleMessage_proto_goTypes,
-		DependencyIndexes: file_proto_SimpleMessage_proto_depIdxs,
-		MessageInfos:      file_proto_SimpleMessage_proto_msgTypes,
+		GoTypes:           file_proto_V2_SimpleMessage_proto_goTypes,
+		DependencyIndexes: file_proto_V2_SimpleMessage_proto_depIdxs,
+		MessageInfos:      file_proto_V2_SimpleMessage_proto_msgTypes,
 	}.Build()
-	File_proto_SimpleMessage_proto = out.File
-	file_proto_SimpleMessage_proto_rawDesc = nil
-	file_proto_SimpleMessage_proto_goTypes = nil
-	file_proto_SimpleMessage_proto_depIdxs = nil
+	File_proto_V2_SimpleMessage_proto = out.File
+	file_proto_V2_SimpleMessage_proto_rawDesc = nil
+	file_proto_V2_SimpleMessage_proto_goTypes = nil
+	file_proto_V2_SimpleMessage_proto_depIdxs = nil
 }
